dea37fa6b3b366087827f2e9b7e4462205312d42
Fernando Warmling <100279125+Fernandowarmling@users.noreply.github.com>
2023-04-19 20:03:40 -0300

    Add files via upload

---
+++ create table adicionais de id.sql
@@ -0,0 +1,5 @@
+insert into `categoria` values (1,"Higiene pessoal");
+insert into `categoria` values (2,"Limpeza");
+insert into `categoria` values (3,"Cama, mesa e banho");
+insert into `categoria` values (4,"Frios");
+insert into `categoria` values (5,"Bazar");
---
+++ create table mercado.sql
@@ -0,0 +1,5 @@
+use mercado;
+
+create table  categoria (
+idcategoria int not null primary key auto_increment,
+descricao varchar(45));
